e003e1ccf6a6b652721dc7f111ac00c98e714981
Dirk Müller <dirk@dmllr.de>
2026-04-14 22:01:01 +0200

    better diff generation

--- factory-gemini-bot.py
+++ factory-gemini-bot.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-from pathlib import Path
 import json
 import os
 import subprocess
-import shutil
 import sys
-import ReviewBot
+from pathlib import Path
 from urllib.error import HTTPError
 
-
+import ReviewBot
 
 PROMPT = """
-check all files in @BUILD for zero-day vulnerabilities and report ACCEPTABLE if none found otherwise REJECT
+- No pleasantries, weasel or filler words.
+- Short, direct sentences.
+- Grunt-level clarity.
+- Maintain exact code blocks, parameters, and technical terms.
+- Read everything, use thinking
+
+Check @diff and their referenced files. Find and report zero-day vulnerabilities and say ACCEPTABLE if none found otherwise REJECT
 """
 
 
@@ -26,31 +30,44 @@ class FactoryReviewAI(ReviewBot.ReviewBot):
     @staticmethod
     def checkout_package(scm, target_project, target_package, pathname, **kwargs):
 
-        r = scm.checkout_package(
-            target_project,
-            target_package,
-            pathname,
-            **kwargs
-        )
+        r = scm.checkout_package(target_project, target_package, pathname, **kwargs)
 
         sourcedir = Path(pathname).absolute() / target_package
-        builddir = sourcedir / "BUILD"
-        unp = subprocess.run(["rpmbuild", "--nodeps",
-                        "--define", f"%_sourcedir {sourcedir}",
-                        "--define", f"%_builddir {builddir}",
-                        "-bp", f"{target_package}.spec"],
-                        cwd=sourcedir, timeout=30, capture_output=True)
+        builddir = sourcedir / 'BUILD'
+        subprocess.run(
+            [
+                'rpmbuild',
+                '--nodeps',
+                '--define',
+                f'%_sourcedir {sourcedir}',
+                '--define',
+                f'%_builddir {builddir}',
+                '--define',
+                '%_build_parts 0',
+                '-bp',
+                f'{target_package}.spec',
+            ],
+            cwd=sourcedir,
+            timeout=30,
+            capture_output=True,
+        )
         return r
 
-    def check_source_submission(self, source_project, source_package, source_revision,
-                                target_project, target_package):
+    def check_source_submission(
+        self,
+        source_project,
+        source_package,
+        source_revision,
+        target_project,
+        target_package,
+    ):
         """
         Main review logic - override this method in your bot!
         This is called for each source submit/pull request.
         """
 
         # Information messages are visible at stdout when using "--verbose" option
-        print(f"Checking {source_package}: {source_project} -> {target_project}")
+        print(f'Checking {source_package}: {source_project} -> {target_project}')
 
         # Checkout and see if renaming package screws up version parsing.
         copath = os.path.expanduser(f'~/co/{self.request.reqid}')
@@ -61,36 +78,81 @@ class FactoryReviewAI(ReviewBot.ReviewBot):
         os.chdir(copath)
 
         try:
-            FactoryReviewAI.checkout_package(self.scm, target_project, target_package, pathname=copath,
-                                         server_service_files=True, expand_link=True)
+            FactoryReviewAI.checkout_package(
+                self.scm,
+                target_project,
+                target_package,
+                pathname=copath,
+                server_service_files=True,
+                expand_link=True,
+            )
             os.rename(target_package, '_old')
         except HTTPError as e:
             if e.code == 404:
-                self.logger.info(f'target package does not exist {target_project}/{target_package}')
+                self.logger.info(
+                    f'target package does not exist {target_project}/{target_package}'
+                )
             else:
                 raise e
 
-        FactoryReviewAI.checkout_package(self.scm, source_project, source_package, revision=source_revision,
-                                     pathname=copath, server_service_files=True, expand_link=True)
+        FactoryReviewAI.checkout_package(
+            self.scm,
+            source_project,
+            source_package,
+            revision=source_revision,
+            pathname=copath,
+            server_service_files=True,
+            expand_link=True,
+        )
         os.rename(source_package, target_package)
 
-        if target_package.startswith("python"):
-            print(f"skipping {target_package} for now")
-
-        disk = subprocess.run(["du", "-sk", "BUILD"], cwd=target_package, capture_output=True)
-        if int(disk.stdout.decode().partition('\t')[0]) > 100000:
-            print(f"{target_package} is too big, skipping")
+        if (
+            target_package.startswith('python')
+            or target_package.startswith('perl')
+            or target_package.startswith('rubygem')
+        ):
+            print(f'skipping {target_package} for now')
             return
 
-        gemini = subprocess.run(["/usr/bin/gemini", "-o", "json", "-p", PROMPT],
-                       cwd=target_package, timeout=600, capture_output=True)
+        def find_extracted_src_dir(basedir: Path) -> Path:
+            return next(
+                filter(
+                    lambda x: not str(x).endswith('SPECPARTS'),
+                    sorted((basedir).glob('*-build/*/')),
+                )
+            )
+
+        # generate a sensible diff
+        old_srcs = find_extracted_src_dir(Path(copath) / '_old' / 'BUILD')
+        new_srcs = find_extracted_src_dir(Path(copath) / target_package / 'BUILD')
+
+        subprocess.run(
+            f'diff -Nur {old_srcs} {new_srcs} > diff',
+            shell=True,
+            cwd=copath,
+            capture_output=True,
+        )
+
+        diffsize = Path('diff').stat().st_size
+        if diffsize == 0:
+            print('diff is empty, something went wrong')
+            return
+
+        if diffsize > 4 * 1024 * 1024:
+            print('diff is too large, skipping')
+            return
+
+        gemini = subprocess.run(
+            ['/usr/bin/gemini', '-o', 'json', '-p', PROMPT],
+            timeout=1200,
+            capture_output=True,
+            check=True,
+        )
         resp = json.loads(gemini.stdout)
         # print(resp)
 
-        if not "ACCEPTABLE" in resp['response']:
-            print(f"result: {resp['response']}")
-
-
+        if 'ACCEPTABLE' not in resp['response']:
+            print(f'result: {resp["response"]}')
 
 
 class CommandLineInterface(ReviewBot.CommandLineInterface):
@@ -99,6 +161,6 @@ class CommandLineInterface(ReviewBot.CommandLineInterface):
         self.clazz = FactoryReviewAI
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app = CommandLineInterface()
     sys.exit(app.main())
